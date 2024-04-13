138bd9f3a3732223e931e7cde06d254db170d50f
alex.popa31 <alex.popa@rawfish.com>
2024-04-13 15:22:03 +0200

    LoadingView - implemented

--- Beyes/Views/Components/LoadingView.swift
+++ Beyes/Views/Components/LoadingView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+struct Loader: View {
+    @State private var isAnimating = false
+
+    var body: some View {
+        Circle()
+            .trim(from: 0, to: 0.7)
+            .stroke(
+                AngularGradient(
+                    gradient: Gradient(colors: [Color.blue, Color.cyan]),
+                    center: .center
+                ),
+                style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round)
+            )
+            .frame(width: 40, height: 40)
+            .rotationEffect(Angle(degrees: isAnimating ? 360 : 0))
+            .onAppear {
+                withAnimation(.linear(duration: 1).repeatForever(autoreverses: false)) {
+                    self.isAnimating = true
+                }
+            }
+    }
+}
+
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Loader()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black.opacity(0.2))
     }
 }
 
