38fe128b1203bc34bbb5012c2a59236bf81a3afb
alex.popa31 <alex.popa@rawfish.com>
2024-04-13 15:21:53 +0200

    Buttons - custom styles

--- Beyes/Views/Components/Buttons.swift
+++ Beyes/Views/Components/Buttons.swift
@@ -6,3 +6,108 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum ButtonStyleType {
+    case small
+    case regular
+    case pill
+
+    var height: CGFloat {
+        switch self {
+        case .regular:
+            48.0
+        case .small:
+            32.0
+        case .pill:
+            36.0
+        }
+    }
+
+    var fontSize: CGFloat {
+        switch self {
+        case .regular:
+            16
+        case .small:
+            16
+        case .pill:
+            14
+        }
+    }
+}
+
+struct Primary: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    let type: ButtonStyleType
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, minHeight: type.height)
+            .frame(height: type.height)
+            .background(Asset.Colors.primary.swiftUIColor)
+            .contentShape(Rectangle())
+            .cornerRadius(200)
+            .font(.system(size: type.fontSize))
+    }
+}
+
+struct Secondary: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    let type: ButtonStyleType
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, minHeight: type.height)
+            .frame(height: type.height)
+            .background(Asset.Colors.secondary.swiftUIColor)
+            .contentShape(Rectangle())
+            .cornerRadius(200)
+            .font(.system(size: type.fontSize))
+    }
+}
+
+struct Tertiary: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    let type: ButtonStyleType
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, minHeight: type.height)
+            .frame(height: type.height)
+            .background(Asset.Colors.tertiary.swiftUIColor)
+            .contentShape(Rectangle())
+            .cornerRadius(200)
+            .font(.system(size: type.fontSize))
+    }
+}
+
+struct Destructive: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    let type: ButtonStyleType
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, minHeight: type.height)
+            .frame(height: type.height)
+            .background(.blue)
+            .contentShape(Rectangle())
+            .cornerRadius(200)
+            .font(.system(size: type.fontSize))
+            .foregroundStyle(.threateningRed)
+    }
+}
+
+struct Clear: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 16))
+            .modifier(ClearModifier())
+    }
+}
+
+struct ClearModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .frame(height: 48)
+            .background(.clear)
+    }
+}
