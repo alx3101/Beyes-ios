9486b017a27bac73c489ed4ac6b66ebf614511c7
alex.popa31 <alex.popa@rawfish.com>
2024-04-26 00:21:47 +0200

    Custom textfield

--- Beyes/Views/Components/TextField.swift
+++ Beyes/Views/Components/TextField.swift
@@ -10,25 +10,34 @@ import SwiftUI
 
 struct CustomTextField: View {
     @Binding var text: String
+    let topTitle: String?
     let placeholder: String
 
-    init(text: Binding<String>, placeholder: String) {
+    init(text: Binding<String>,
+         topTitle: String? = nil,
+         placeholder: String) {
         _text = text
+        self.topTitle = topTitle
         self.placeholder = placeholder
     }
 
     var body: some View {
-        HStack {
+        VStack(alignment: .leading, spacing: 4) {
+            if let topTitle {
+                Text(topTitle)
+                    .padding(.leading, 4)
+                    .font(.system(size: 15))
+                    .foregroundStyle(.gray)
+            }
             TextField(placeholder, text: $text)
                 .font(.system(size: 16))
-
-            Spacer()
-        }
-        .padding(.horizontal, 15)
-        .frame(maxWidth: .infinity, maxHeight: 40)
-        .background {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.regularMaterial)
+                .padding(.horizontal, 15)
+                .frame(maxWidth: .infinity)
+                .frame(height: 40)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.regularMaterial)
+                }
         }
     }
 }
@@ -36,41 +45,58 @@ struct CustomTextField: View {
 struct CustomSecureField: View {
     @Binding var text: String
     @State private var passwordHidden: Bool = true
+    let topTitle: String?
     let placeholder: String
 
-    init(text: Binding<String>, placeholder: String) {
+    init(text: Binding<String>,
+         topTitle: String? = nil,
+         placeholder: String) {
         _text = text
+        self.topTitle = topTitle
         self.placeholder = placeholder
     }
 
     var body: some View {
-        HStack {
-            if passwordHidden {
-                SecureField(placeholder, text: $text)
-                    .font(.system(size: 16))
-
-            } else {
-                TextField(placeholder, text: $text)
-                    .font(.system(size: 16))
+        VStack(alignment: .leading, spacing: 4) {
+            if let topTitle {
+                Text(topTitle)
+                    .padding(.leading, 4)
+                    .font(.system(size: 15))
+                    .foregroundStyle(.gray)
             }
 
-            Spacer()
+            HStack {
+                if passwordHidden {
+                    SecureField(placeholder, text: $text)
+                        .font(.system(size: 16))
 
-            Button { passwordHidden.toggle() } label: {
-                Image(systemName: passwordHidden ? "eye" : "eye.slash")
-                    .renderingMode(.template)
-                    .tint(.gray)
+                } else {
+                    TextField(placeholder, text: $text)
+                        .font(.system(size: 16))
+                }
+
+                Spacer()
+
+                Button { passwordHidden.toggle() } label: {
+                    Image(systemName: passwordHidden ? "eye" : "eye.slash")
+                        .renderingMode(.template)
+                        .tint(.gray)
+                }
             }
-        }
-        .padding(.horizontal, 15)
-        .frame(maxWidth: .infinity, maxHeight: 40)
-        .background {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.regularMaterial)
+            .padding(.horizontal, 15)
+            .frame(maxWidth: .infinity)
+            .frame(height: 40)
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.regularMaterial)
+            }
         }
     }
 }
 
 #Preview {
-    CustomSecureField(text: .constant(""), placeholder: "")
+    VStack {
+        CustomTextField(text: .constant("test"), topTitle: "Top", placeholder: "")
+        CustomSecureField(text: .constant("test"), topTitle: "Top", placeholder: "")
+    }
 }
