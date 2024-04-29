0dbc62b359e2877964b3786c3fe3651ed1cd423b
alex.popa31 <alex.popa@rawfish.com>
2024-04-30 00:02:03 +0200

    String - email & password validation

--- Beyes/Extension/Strings.swift
+++ Beyes/Extension/Strings.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension String {
+    func isValidEmail() -> Bool {
+        // Regex pattern per verificare il formato dell'email
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+
+        // Crea un predicato con il pattern regex
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+
+        // Verifica se la stringa corrisponde al pattern regex
+        return emailPredicate.evaluate(with: self)
+    }
+
+    func isValidPassword() -> Bool {
+        let passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+    }
+}
