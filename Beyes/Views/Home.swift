e4fa3e86a90ab62924b3a2b4ae463b6a71cf8504
alex.popa31 <alex.popa@rawfish.com>
2024-05-01 20:36:28 +0200

    Home  - Logout logic

--- Beyes/Views/Home.swift
+++ Beyes/Views/Home.swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct HomeView: View {
     @Environment(\.appEnvironment) var appEnvironment
     @EnvironmentObject var router: Router
+    @State private var action: Loadable<Void> = .notRequested
 
     var body: some View {
         VStack {
             Text("home")
-            Button { router.setMain(.registration) } label: {
-                Text("main")
+            Button { appEnvironment.interactors.auth.logout {
+                action = $0
+            } } label: {
+                Text("login")
             }
+            .onReceive(appEnvironment.interactors.auth.$currentSession, perform: { value in
+                if !(value ?? false) {
+                    router.setMain(.login)
+                }
+
+            })
 //            Button("Logout") {
 //                appEnvironment.viewModels.authentication.logout { completion in
 //                    switch completion {
