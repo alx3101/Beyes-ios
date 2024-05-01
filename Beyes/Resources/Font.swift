00c02bc1e7980372dec586933516e358b1d9fbf9
alex.popa31 <alex.popa@rawfish.com>
2024-05-01 20:34:40 +0200

    FontStyles

--- Beyes/Resources/Font.swift
+++ Beyes/Resources/Font.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Text {
+    // Font weight = 300
+    func light(size: CGFloat = 16, color: Color = .white) -> Self {
+        font(.system(size: size, weight: .light))
+            .foregroundColor(color)
+    }
+
+    // Font weight = 400
+    func regular(size: CGFloat = 16, color: Color = .white) -> Self {
+        font(.system(size: size, weight: .regular))
+            .foregroundColor(color)
+    }
+
+    // Font weight = 500
+    func medium(size: CGFloat = 18, color: Color = .white) -> Self {
+        font(.system(size: size, weight: .medium))
+            .foregroundColor(color)
+    }
+
+    // Font weight = 600
+    func semiBold(size: CGFloat = 18, color: Color = .white) -> some View {
+        font(.system(size: size, weight: .semibold))
+            .foregroundColor(color)
+    }
+
+    // Font weight = 700
+    func bold(size: CGFloat = 18, color: Color = .white) -> some View {
+        font(.system(size: size, weight: .bold))
+            .foregroundColor(color)
+    }
+}
